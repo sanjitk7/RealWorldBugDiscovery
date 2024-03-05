b5cab1bb99332e48ed732b419c173f459adad202
Sakshil Verma <sakshil2@illinois.edu>
2024-03-05 12:16:23 -0600

    Bears bugs and scripts added

---
+++ Bugs/Bears/Scripts/Bears_generate_results.py
@@ -0,0 +1,26 @@
+import os
+import csv
+import Bears_Benchmark
+
+
+valid_metrics = ["CChange", "MChange", "LChange", "LD", "CB", "CP", "CC", "CodeBLEU"]
+results_csv = "./results/Bears_Results.csv"
+
+if __name__ == "__main__":
+    bugs = os.listdir("./Bugs")
+    bugs = [bug for bug in bugs if os.path.isdir(f"./Bugs/{bug}")]
+
+    column_names = ["Bug Id", "CChange", "MChange", "LChange", "LD", "CB", "CP", "CC", "CodeBLEU"]
+    with open(results_csv, mode='a', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames=column_names)
+
+        if file.tell() == 0:
+            writer.writeheader()
+
+        for bug in bugs:
+            row = {}
+            row["Bug Id"] = bug
+            for metric in valid_metrics:
+                row[metric] = Bears_Benchmark.get_metric("Bears", bug, metric)
+            writer.writerow(row)
+
