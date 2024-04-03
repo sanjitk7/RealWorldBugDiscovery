3ef32e330c53b7f279dfc33c8e0858c850d52885
Ajitesh Nair <ajitesh4@illinois.edu>
2024-04-03 02:31:22 -0500

    Quixbugs: Milestone 3

---
+++ QuixBugs/BREADTH_FIRST_SEARCH/Buggy-Version/java_testcases/randoop/RandoopRegressionTestBREADTH_FIRST_SEARCHBuggy.java
@@ -0,0 +1,8 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ RandoopRegressionTestBREADTH_FIRST_SEARCHBuggy0.class })
+public class RandoopRegressionTestBREADTH_FIRST_SEARCHBuggy {
+}
+
